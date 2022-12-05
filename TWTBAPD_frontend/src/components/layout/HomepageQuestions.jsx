cc3d8d38830df32ee7b5c6d6325d1d5ff39d90ed
Veniamin Polienko <kasaklalita@mail.ru>
2022-12-06 02:56:32 +0300

    Added basic fetching questions from the backend

--- TWTBAPD_frontend/src/components/layout/HomepageQuestions.jsx
+++ TWTBAPD_frontend/src/components/layout/HomepageQuestions.jsx
@@ -1,48 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const HomepageQuestions = () => {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    const fetchAllQuestions = async () => {
+      await axios
+        .get("http://127.0.0.1:8000/api/v1/get-all-questions/")
+        .then((response) => {
+          setQuestions(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    fetchAllQuestions();
+  }, []);
+
   return (
     <div className="content">
       <div className="box content">
         <h1>Общие вопросы по Python</h1>
         <p>В этой секции находятся общие вопросы об основах Python</p>
         <ol>
-          <li>
-            <a>Python - интерпретируемый язык или компилируемый?</a>
-          </li>
-          <li>
-            <a>Какие есть меняющиеся и постоянные типы данных?</a>
-          </li>
-          <li>
-            <a>Что такое область видимости переменных?</a>
-          </li>
-          <li>
-            <a>Какие знаете принципы программирования?</a>
-          </li>
-          <li>
-            <a>Какие знаете принципы программирования?</a>
-          </li>
-        </ol>
-      </div>
-      <div className="box content">
-        <h1>Вопросы по Django</h1>
-        <p>В этой секции находятся общие вопросы об основах Python</p>
-        <ol>
-          <li>
-            <a>Python - интерпретируемый язык или компилируемый?</a>
-          </li>
-          <li>
-            <a>Какие есть меняющиеся и постоянные типы данных?</a>
-          </li>
-          <li>
-            <a>Что такое область видимости переменных?</a>
-          </li>
-          <li>
-            <a>Какие знаете принципы программирования?</a>
-          </li>
-          <li>
-            <a>Какие знаете принципы программирования?</a>
-          </li>
+          {questions.map((question) => {
+            return (
+              <li key={question.id}>
+                <a>{question["question_text"]}</a>
+              </li>
+            );
+          })}
         </ol>
       </div>
     </div>
