199b081513313464c47540111fa2bf21f51c0013
Veniamin Polienko <kasaklalita@mail.ru>
2022-12-06 02:28:28 +0300

    Created basic models and added some questions to the database

--- TWTBAPD_backend/api/models.py
+++ TWTBAPD_backend/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'{self.slug}/'
+
+
+class Question(models.Model):
+    category = models.ForeignKey(Category, related_name='questions', on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=128)
+    answer = models.TextField()
+    link = models.CharField(max_length=128)
+    dateAdded = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question_text
