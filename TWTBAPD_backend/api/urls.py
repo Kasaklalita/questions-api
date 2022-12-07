51bce6bdb480d48707537a8de99becc14e116c99
Veniamin Polienko <kasaklalita@mail.ru>
2022-12-07 23:32:19 +0300

    Changed some APIs

--- TWTBAPD_backend/api/urls.py
+++ TWTBAPD_backend/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('get-all-categories/', GetAllCategories.as_view()),
-    path('get-all-questions/', GetAllQuestions.as_view()),
-    path('get-questions-by-category/', GetQuestionsByCategory.as_view()),
-    path('get-question-detail', GetQuestionDetail.as_view())
+    path('categories/', GetAllCategories.as_view()),
+    path('categories/<slug:category_slug>', GetQuestionsByCategory.as_view()),
+    path('questions/', GetAllQuestions.as_view()),
+    path('questions/<int:id>', GetQuestionDetail.as_view())
 ]
